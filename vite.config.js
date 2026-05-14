c30e6452bfdb8dc4fb2e5463be8dff72e3640a28
Chenlin <107831437+iren1ing@users.noreply.github.com>
2026-05-14 17:51:28 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+   import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/lynn.portfolio/', 
-})
+   export default defineConfig({
+     plugins: [react()],
+     base: '/lynn.profile/',  
+   })
